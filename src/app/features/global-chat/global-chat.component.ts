f2b961987e51f2d090de9041b9aed898e507632c
peter <peter.berghuis_13@hotmail.com>
2023-06-05 22:10:54 +0200

    fixed height 100% issue and better chat visuals

--- src/app/features/global-chat/global-chat.component.ts
+++ src/app/features/global-chat/global-chat.component.ts
@@ -1,28 +1,38 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Message } from "../../shared/models/message.model";
 import { FormControl } from "@angular/forms";
 import { ChatService } from "../../shared/chat.service";
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from "@angular/material/form-field";
 
 @Component({
   selector: 'app-global-chat',
   templateUrl: './global-chat.component.html',
-  styleUrls: ['./global-chat.component.scss']
+  styleUrls: ['./global-chat.component.scss'],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {
+        subscriptSizing: 'dynamic'
+      }
+    }
+  ]
 })
 export class GlobalChatComponent implements OnInit, OnDestroy {
 
   connection;
   messages: (Message | string)[] = [];
   inputFormControl: FormControl;
+  @ViewChild('chatMessagesContainer') chatMessagesContainer!: ElementRef;
 
-  constructor(public chatService: ChatService) {
+  constructor(public chatService: ChatService, private changeDetectorRef: ChangeDetectorRef) {
     this.inputFormControl = new FormControl<string>('');
     this.connection = chatService.connect();
   }
 
   ngOnInit(): void {
     this.chatService.joinChat('globalChat');
-    this.chatService.message.subscribe(message => this.messages.push(message));
-    this.chatService.systemNotification.subscribe(message => this.messages.push(message));
+    this.chatService.message.subscribe(message => this.addNewMessage(message));
+    this.chatService.systemNotification.subscribe(message => this.addNewMessage(message));
   }
 
   ngOnDestroy(): void {
@@ -31,10 +41,21 @@ export class GlobalChatComponent implements OnInit, OnDestroy {
 
   onEnter() {
     const value = this.inputFormControl.getRawValue();
-    if (value) {
-      this.chatService.sendMessage('globalChat', value);
+    if (value.trim()) {
+      this.chatService.sendMessage('globalChat', value.trim());
       this.inputFormControl.reset();
     }
+  }
+
+  private addNewMessage(message: string | Message) {
+    this.messages.push(message);
+    this.changeDetectorRef.detectChanges();
+    this.scrollMessagesToBottom();
+  }
+
+  private scrollMessagesToBottom() {
+    const container = this.chatMessagesContainer.nativeElement;
+    container.scrollTop = container.scrollHeight;
   }
 
   isString(item: Message | string): item is string {
