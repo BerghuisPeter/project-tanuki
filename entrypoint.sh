19242b7b03523d3aea8328376551c7cc1e8913ca
peter <peter.berghuis_13@hotmail.com>
2026-03-23 22:50:21 +0900

    env at build time replacement with runtime config loading.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,20 @@
 
 # Set the path to the config file
 CONFIG_FILE="/usr/share/nginx/html/assets/config.json"
+NGINX_CONF_TEMPLATE="/etc/nginx/templates/nginx.conf.template"
+NGINX_CONF="/etc/nginx/nginx.conf"
+
+# Ensure PORT environment variable is set for GCP and substitution
+if [ -z "$PORT" ]; then
+  export PORT=8080
+fi
+
+# Run envsubst on nginx config template
+# We only want to substitute ${PORT} in the nginx configuration
+# The standard nginx docker image logic expects templates in /etc/nginx/templates/
+# and it outputs them to /etc/nginx/conf.d/.
+# But since variable-nginx.conf is a FULL nginx.conf, we should overwrite /etc/nginx/nginx.conf.
+envsubst '${PORT}' < "$NGINX_CONF_TEMPLATE" > "$NGINX_CONF"
 
 # Start the JSON object
 echo "{" > $CONFIG_FILE
