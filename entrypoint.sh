1e6be131f54c66281c9caba5fd86b47651444413
peter <peter.berghuis_13@hotmail.com>
2026-03-24 09:41:21 +0900

    Extend `entrypoint.sh` to replace `PORT` in Nginx configuration and refine Angular environment variable substitution.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,13 @@
 # Dynamically collect all NG_APP_ env vars for envsubst
 NG_APP_VARS=$(env | grep '^NG_APP_' | cut -d= -f1 | sed 's/^/\${/' | sed 's/$/}/' | tr '\n' ',')
 
+# Replace Angular environment variables in index.html
 envsubst "$NG_APP_VARS" \
   < /usr/share/nginx/html/index.html \
   > /usr/share/nginx/html/index.html.tmp && \
   mv /usr/share/nginx/html/index.html.tmp /usr/share/nginx/html/index.html
+
+# Replace PORT and other environment variables in Nginx configuration
+envsubst '${PORT}' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
