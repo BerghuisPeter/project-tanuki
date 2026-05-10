f5b39e191f2e2f85982d600f6ff6879ea7d41f32
peter <peter.berghuis_13@hotmail.com>
2026-05-10 22:14:21 +0900

    Refactor `entrypoint.sh` to dynamically process environment variables for all localized `index.html` files via loop.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 # Dynamically collect all NG_APP_ env vars for envsubst
 NG_APP_VARS=$(env | grep '^NG_APP_' | cut -d= -f1 | sed 's/^/\${/' | sed 's/$/}/' | tr '\n' ',')
 
-# Replace Angular environment variables in index.html for each language
-envsubst "$NG_APP_VARS" \
-  < /usr/share/nginx/html/en/index.html \
-  > /usr/share/nginx/html/en/index.html.tmp && \
-  mv /usr/share/nginx/html/en/index.html.tmp /usr/share/nginx/html/en/index.html
-
-envsubst "$NG_APP_VARS" \
-  < /usr/share/nginx/html/fr/index.html \
-  > /usr/share/nginx/html/fr/index.html.tmp && \
-  mv /usr/share/nginx/html/fr/index.html.tmp /usr/share/nginx/html/fr/index.html
+# Replace Angular environment variables in index.html for each language directory
+for dir in /usr/share/nginx/html/*/; do
+    if [ -f "$dir/index.html" ]; then
+        echo "Processing $dir/index.html"
+        envsubst "$NG_APP_VARS" \
+          < "$dir/index.html" \
+          > "$dir/index.html.tmp" && \
+          mv "$dir/index.html.tmp" "$dir/index.html"
+    fi
+done
 
 
 # Replace PORT and other environment variables in Nginx configuration
