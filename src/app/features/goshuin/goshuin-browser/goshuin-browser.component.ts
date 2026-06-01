a4a04ed147cdc63ce070cd609cb3a708a16273aa
peter <peter.berghuis_13@hotmail.com>
2026-06-01 16:55:09 +0900

    added url params for filtering and sorting

--- src/app/features/goshuin/goshuin-browser/goshuin-browser.component.ts
+++ src/app/features/goshuin/goshuin-browser/goshuin-browser.component.ts
@@ -1,8 +1,8 @@
-import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { TranslocoDirective } from "@jsverse/transloco";
 import { APP_PATHS } from "../../../shared/models/app-paths.model";
 import { MatIcon } from "@angular/material/icon";
-import { RouterLink } from "@angular/router";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { MatButton } from "@angular/material/button";
 import { MatFormField, MatInputModule } from "@angular/material/input";
 import { Goshuin, GoshuinGoshuinService } from "../../../../openApi/goshuin";
@@ -13,6 +13,9 @@ import { MatChipListbox, MatChipOption } from "@angular/material/chips";
 import { MatDivider } from "@angular/material/list";
 import { FilterContainerComponent } from "./filter-container.component/filter-container.component";
 import { MatFormFieldModule } from "@angular/material/form-field";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { distinctUntilChanged } from "rxjs";
+import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-goshuin-browser',
@@ -29,25 +32,65 @@ import { MatFormFieldModule } from "@angular/material/form-field";
     MatChipListbox,
     MatDivider,
     FilterContainerComponent,
+    ReactiveFormsModule,
   ],
   templateUrl: './goshuin-browser.component.html',
   styleUrl: './goshuin-browser.component.scss',
 })
-export class GoshuinBrowserComponent implements OnInit {
+export class GoshuinBrowserComponent {
   readonly fullMapLink = ['/', APP_PATHS.GOSHUIN, APP_PATHS.GOSHUIN_MAP];
   isLoadingQuery = signal(true);
   searchResults = signal<Goshuin[]>([]);
   private readonly goshuinService = inject(GoshuinGoshuinService);
   private readonly snackBar = inject(MatSnackBar);
   private readonly languageService = inject(LanguageService);
+  private readonly fb = inject(FormBuilder);
+  filterForm = this.fb.group({
+    search: [''],
+    affiliation: [''],
+    type: [''],
+    pages: [''],
+    sortBy: ['date']
+  });
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+
   currentLocale = computed(() => this.languageService.currentLocale().slice(0, 2));
 
-  ngOnInit(): void {
-    this.search();
+  constructor() {
+    this.filterForm.valueChanges.pipe(
+      distinctUntilChanged((a, b) => JSON.stringify(a) === JSON.stringify(b)),
+      takeUntilDestroyed()
+    ).subscribe((values) => {
+
+      console.log('values:', values);
+
+      const queryParams = Object.fromEntries(
+        Object.entries(values).filter(
+          ([_, v]) => v !== null && v !== undefined && v !== ''
+        )
+      );
+
+      console.log('queryParams:', queryParams);
+
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: queryParams,
+        queryParamsHandling: 'replace',
+      });
+    });
+
+    this.route.queryParams.pipe(
+      takeUntilDestroyed()
+    ).subscribe(params => {
+      this.filterForm.patchValue(params, { emitEvent: false });
+      this.search();
+    });
   }
 
   private search() {
     this.isLoadingQuery.set(true);
+
     this.goshuinService.getGoshuins().subscribe({
       next: (goshuins) => {
         this.searchResults.set(goshuins);
