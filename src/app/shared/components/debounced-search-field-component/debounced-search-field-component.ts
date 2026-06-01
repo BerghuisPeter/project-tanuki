0682c25519245d6d53bf2d324c619deafbec5c57
peter <peter.berghuis_13@hotmail.com>
2026-06-01 19:41:22 +0900

    debounce input v2

--- src/app/shared/components/debounced-search-field-component/debounced-search-field-component.ts
+++ src/app/shared/components/debounced-search-field-component/debounced-search-field-component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component, DestroyRef, effect, inject, input, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, forwardRef, input, signal } from '@angular/core';
 import { NgStyle } from "@angular/common";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { debounceTime } from "rxjs";
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { toObservable } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-debounced-search-field-component',
@@ -16,42 +16,74 @@ import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
     MatInputModule,
     MatIconModule,
   ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DebouncedSearchFieldComponent),
+      multi: true,
+    },
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './debounced-search-field-component.html',
   styleUrl: './debounced-search-field-component.scss',
 })
 export class DebouncedSearchFieldComponent {
-  readonly control = input.required<FormControl>();
   readonly label = input('Search');
   readonly placeholder = input('');
   readonly icon = input('search');
   readonly debounceMs = input(500);
 
+  readonly value = signal('');
+  readonly disabled = signal(false);
   readonly animating = signal(false);
 
-  private readonly destroyRef = inject(DestroyRef);
+  private readonly value$ = toObservable(this.value);
 
   constructor() {
     effect(() => {
-      const control = this.control();
       const debounceMs = this.debounceMs();
 
-      control.valueChanges
-        .pipe(
-          takeUntilDestroyed(this.destroyRef),
-          debounceTime(debounceMs),
-        )
+      const sub = this.value$
+        .pipe(debounceTime(debounceMs))
         .subscribe(() => {
           this.animating.set(false);
         });
 
-      control.valueChanges
-        .pipe(takeUntilDestroyed(this.destroyRef))
-        .subscribe(() => {
-          this.restartAnimation();
-        });
+      return () => sub.unsubscribe();
     });
   }
+
+  writeValue(value: string | null): void {
+    this.value.set(value ?? '');
+  }
+
+  registerOnChange(fn: (value: string) => void): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(disabled: boolean): void {
+    this.disabled.set(disabled);
+  }
+
+  onInput(value: string): void {
+    this.restartAnimation();
+    this.value.set(value);
+    this.onChange(value);
+  }
+
+  onBlur(): void {
+    this.onTouched();
+  }
+
+  private onChange: (value: string) => void = () => {
+  };
+
+  private onTouched: () => void = () => {
+  };
 
   private restartAnimation(): void {
     this.animating.set(false);
