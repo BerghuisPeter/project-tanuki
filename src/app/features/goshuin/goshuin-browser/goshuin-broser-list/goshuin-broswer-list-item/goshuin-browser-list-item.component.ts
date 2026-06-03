44672362bb09bb2493badc904de1394a4b1fd8f4
peter <peter.berghuis_13@hotmail.com>
2026-06-03 22:19:22 +0900

    refined goshuin browser layout with responsive grid, enhanced list item structure and styling

--- src/app/features/goshuin/goshuin-browser/goshuin-broser-list/goshuin-broswer-list-item/goshuin-browser-list-item.component.ts
+++ src/app/features/goshuin/goshuin-browser/goshuin-broser-list/goshuin-broswer-list-item/goshuin-browser-list-item.component.ts
@@ -2,12 +2,14 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Goshuin } from "../../../../../../openApi/goshuin";
 import { MatIcon } from "@angular/material/icon";
 import { MatButton } from "@angular/material/button";
+import { MatDivider } from "@angular/material/list";
 
 @Component({
   selector: 'app-goshuin-browser-list-item',
   imports: [
     MatIcon,
-    MatButton
+    MatButton,
+    MatDivider
   ],
   templateUrl: './goshuin-browser-list-item.component.html',
   styleUrl: './goshuin-browser-list-item.component.scss',
