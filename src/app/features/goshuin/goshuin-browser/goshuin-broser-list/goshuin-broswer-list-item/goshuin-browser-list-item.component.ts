b16257efb706763e49f7eb6b2186203401a32b21
peter <peter.berghuis_13@hotmail.com>
2026-06-03 20:41:06 +0900

    updated goshuin browser list item layout with material components; enhanced template structure and styling

--- src/app/features/goshuin/goshuin-browser/goshuin-broser-list/goshuin-broswer-list-item/goshuin-browser-list-item.component.ts
+++ src/app/features/goshuin/goshuin-browser/goshuin-broser-list/goshuin-broswer-list-item/goshuin-browser-list-item.component.ts
@@ -1,9 +1,14 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { Goshuin } from "../../../../../../openApi/goshuin";
+import { MatIcon } from "@angular/material/icon";
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: 'app-goshuin-browser-list-item',
-  imports: [],
+  imports: [
+    MatIcon,
+    MatButton
+  ],
   templateUrl: './goshuin-browser-list-item.component.html',
   styleUrl: './goshuin-browser-list-item.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
