181480f4bfa76a0bac7a1669f62adf9d9bc8064b
peter <peter.berghuis_13@hotmail.com>
2026-06-08 16:37:37 +0900

    goshuin header refactoring

---
+++ src/app/features/goshuin/goshuin-browser/goshuin-browser-header/goshuin-browser-header.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import { GoshuinBrowserService } from "../goshuin-browser.service";
+import { APP_PATHS } from "../../../../shared/models/app-paths.model";
+import { MatOption } from "@angular/material/core";
+import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from "@angular/router";
+import {
+  DebouncedSearchFieldComponent
+} from "../../../../shared/components/debounced-search-field/debounced-search-field.component";
+import { MatFormField, MatLabel } from "@angular/material/input";
+import { MatSelect } from "@angular/material/select";
+import { TranslocoDirective } from "@jsverse/transloco";
+import { GoshuinSort } from "../../../../../openApi/goshuin";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatButton } from "@angular/material/button";
+
+@Component({
+  selector: 'app-goshuin-browser-header',
+  imports: [
+    MatOption,
+    MatIcon,
+    RouterLink,
+    DebouncedSearchFieldComponent,
+    MatFormField,
+    MatLabel,
+    MatSelect,
+    TranslocoDirective,
+    ReactiveFormsModule,
+    MatButton
+  ],
+  templateUrl: './goshuin-browser-header.component.html',
+  styleUrl: './goshuin-browser-header.component.scss',
+})
+export class GoshuinBrowserHeaderComponent {
+  readonly fullMapLink = ['/', APP_PATHS.GOSHUIN, APP_PATHS.GOSHUIN_MAP];
+  protected readonly service = inject(GoshuinBrowserService);
+  protected readonly GoshuinSort = GoshuinSort;
+}
--- src/app/features/goshuin/goshuin-browser/goshuin-browser.component.ts
+++ src/app/features/goshuin/goshuin-browser/goshuin-browser.component.ts
@@ -1,45 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { TranslocoDirective } from '@jsverse/transloco';
-import { APP_PATHS } from '../../../shared/models/app-paths.model';
-import { MatIcon } from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
-import { MatButton } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatDivider } from '@angular/material/list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  DebouncedSearchFieldComponent
-} from "../../../shared/components/debounced-search-field/debounced-search-field.component";
 import { GoshuinFilterPanelComponent } from "./goshuin-filter-panel/goshuin-filter-panel.component";
 import { GoshuinBrowserService } from './goshuin-browser.service';
 import { GoshuinBrowserListComponent } from "./goshuin-broser-list/goshuin-browser-list.component";
-import { MatOption, MatSelect } from "@angular/material/select";
-import { GoshuinSort } from "../../../../openApi/goshuin";
+
+import { GoshuinBrowserHeaderComponent } from "./goshuin-browser-header/goshuin-browser-header.component";
 
 @Component({
   selector: 'app-goshuin-browser',
   imports: [
     TranslocoDirective,
-    MatIcon,
-    RouterLink,
-    MatButton,
     MatFormFieldModule,
     MatInputModule,
     MatDivider,
     ReactiveFormsModule,
-    DebouncedSearchFieldComponent,
     GoshuinFilterPanelComponent,
     GoshuinBrowserListComponent,
-    MatSelect,
-    MatOption,
+    GoshuinBrowserHeaderComponent,
   ],
   providers: [GoshuinBrowserService],
   templateUrl: './goshuin-browser.component.html',
   styleUrl: './goshuin-browser.component.scss',
 })
 export class GoshuinBrowserComponent {
-  readonly fullMapLink = ['/', APP_PATHS.GOSHUIN, APP_PATHS.GOSHUIN_MAP];
-  protected readonly service = inject(GoshuinBrowserService);
-  protected readonly GoshuinSort = GoshuinSort;
 }
