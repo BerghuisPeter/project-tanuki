c846e3fd6157568b4a061d382896a4e8e50ca2a0
peter <peter.berghuis_13@hotmail.com>
2026-06-27 20:46:08 +0900

    added GCP dev environment configuration; updated environment setup and build scripts

---
+++ src/environments/environment.gcp-dev.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  isLocal: 'true',
+  NG_APP_SOCKET_SERVER_URL: 'https://dev.socket.project-tanuki.net',
+  NG_APP_AUTH_API_URL: 'https://dev.auth.project-tanuki.net',
+  NG_APP_PROFILE_API_URL: 'https://dev.profile.project-tanuki.net',
+  NG_APP_GOSHUIN_API_URL: 'https://dev.goshuin.project-tanuki.net',
+  NG_APP_TILE_SERVER_URL: 'https://dev.tile.project-tanuki.net',
+  NG_APP_ENABLE_BATTLESHIP: 'true',
+  isGoshuinActiveFlag: 'true',
+};
